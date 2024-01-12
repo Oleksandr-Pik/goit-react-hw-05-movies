5846730f468a8cd4354537efc899f328e65bfbb2
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2024-01-12 14:44:26 +0200

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import Home from 'pages/Home';
+import MovieDetails from 'pages/MovieDetails';
+import Movies from 'pages/Movies';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './NotFound/NotFound';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template goit-react-hw-05-movies
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          {/* <Route path="cast" element={<Cast />} />
+          <Route path="reviews " element={<Reviews />} /> */}
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      Page not found, Sorry!
+    </div>
+  )
+}
+
+export default NotFound
